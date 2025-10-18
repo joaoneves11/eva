435271ac0eae83c0897e213687cce5c1c3a64e02
João Neves <joaopedro.n@outlook.com.br>
2025-10-18 18:29:25 -0300

    feat: adiciona camada de serviços do app (ex: integração e controle)

---
+++ services/carControlService.ts
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+export class CarControlService {
+    baseUrl: string;
+
+    constructor() {
+        this.baseUrl = "http://192.168.4.1";
+    }
+
+    async goForward() {
+        console.log("FRENTE");
+        await axios.get(`${this.baseUrl}/action?go=F`);
+    }
+
+    async goBack() {
+        console.log("TRÁS");
+        await axios.get(`${this.baseUrl}/action?go=B`);
+    }
+
+    async goLeft() {
+        console.log("ESQUERDA");
+        await axios.get(`${this.baseUrl}/action?go=L`);
+    }
+
+    async goRight() {
+        console.log("DIREITA");
+        await axios.get(`${this.baseUrl}/action?go=R`);
+    }
+
+    async stop() {
+        console.log("PARAR");
+        await axios.get(`${this.baseUrl}/action?go=S`);
+    }
+
+    async toggleLight() {
+        console.log("ALTERNAR LUZ");
+        await axios.get(`${this.baseUrl}/action?go=l`);
+    }
+
+    async sendMessage(message: string) {
+        console.log("ENVIAR MENSAGEM:", message);
+        await axios.get(`${this.baseUrl}/action?go=m${message}`);
+    }
+
+    async getStatus() {
+        console.log("BUSCAR STATUS DO POWERBANK");
+        const response = await axios.get(`${this.baseUrl}/status`);
+        return response.data;
+    }
+}
